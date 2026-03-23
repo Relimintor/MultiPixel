e28b3a8b2966dd65197287eefece0815b09965b9
Raptor4 <raptor4mc@gmail.com>
2026-03-23 11:17:49 +0100

    Update config.js

--- game/singleplayer/config.js
+++ game/singleplayer/config.js
@@ -799,7 +799,7 @@
     LIQUID_BLOCKS: [4, 33, 60, 61, 62, 63, 64, 65, 66],
     DEFAULT_PLAYER: {
       moveSpeed: 0.12,
-      sprintMultiplier: 1.15,
+      sprintMultiplier: 1.35,
       rotationSpeed: 0.002,
       health: 20,
       maxHealth: 20,
